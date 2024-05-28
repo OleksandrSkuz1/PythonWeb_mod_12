ab828f2dbc4cd24539755d62f186f549cf582569
OleksandrSkuz1 <138138803+OleksandrSkuz1@users.noreply.github.com>
2024-05-28 05:39:15 +0300

    add  src/routes/contacts.py

--- src/routes/contacts.py
+++ src/routes/contacts.py
@@ -1,58 +1,31 @@
-from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, HTTPException, Depends, status, Path, Query
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from src.database.db import get_db
-from src.schemas.contacts import ContactResponse, ContactBase
-from src.repository import contacts as contacts_repository
-
-router = APIRouter()
-contacts_router = APIRouter()
-birthdays_router = APIRouter()
-
-@router.get("/", response_model=list[ContactResponse])
-async def get_contacts(db: Session = Depends(get_db)):
-    return contacts_repository.get_contacts(db)
 
 
-@router.get("/{contact_id}", response_model=ContactResponse)
-async def get_contact_by_id(contact_id: int, db: Session = Depends(get_db)):
-    contact = contacts_repository.get_contact_by_id(db, contact_id)
-    if not contact:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found")
-    return contact
+router = APIRouter(prefix='contacts', tags=['contacts'])
+
+@router.get("/")
+async def get_todos():
+    pass
 
 
-@router.post("/", response_model=ContactResponse)
-async def create_contact(contact: ContactBase, db: Session = Depends(get_db)):
-    db_contact = contacts_repository.get_contact_by_id(db, contact.id)
-    if db_contact:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Contact already exists.")
-    return contacts_repository.create_contact(db, contact)
+@router.get("/{contact_id}")
+async def get_contact():
+    pass
 
 
-@router.put("/{contact_id}", response_model=ContactResponse)
-async def update_contact(contact_id: int, contact: ContactBase, db: Session = Depends(get_db)):
-    db_contact = contacts_repository.update_contact(db, contact_id, contact)
-    if not db_contact:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found")
-    return db_contact
+@router.post("/")
+async def create_contact():
+    pass
 
 
-@router.delete("/{contact_id}", response_model=ContactResponse)
-async def delete_contact(contact_id: int, db: Session = Depends(get_db)):
-    db_contact = contacts_repository.delete_contact(db, contact_id)
-    if not db_contact:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found")
-    return db_contact
+@router.put("/{contact_id}")
+async def update_contact():
+    pass
 
 
-@router.get("/search/", response_model=list[ContactResponse])
-async def search_contacts(query: str, db: Session = Depends(get_db)):
-    return contacts_repository.search_contacts(db, query)
-
-
-@router.get("/birthdays/", response_model=list[ContactResponse])
-async def get_upcoming_birthdays(db: Session = Depends(get_db)):
-    return contacts_repository.get_upcoming_birthdays(db)
-
-
-
+@router.delete("/{contact_id}")
+async def delete_contact():
+    pass
