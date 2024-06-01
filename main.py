276d53f2bae73f4a9eff13f6fc20be1d83695caa
OleksandrSkuz1 <138138803+OleksandrSkuz1@users.noreply.github.com>
2024-06-01 19:05:58 +0300

    add

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import logging
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -8,17 +7,6 @@ from src.routes import contacts, auth
 
 app = FastAPI()
 
-# Налаштування логування
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[
-        logging.FileHandler("app.log"),  # Запис у файл
-        logging.StreamHandler()  # Відображення у консолі
-    ]
-)
-
-logger = logging.getLogger(__name__)
 
 app.include_router(auth.router, prefix="/api")
 app.include_router(contacts.router, prefix="/api")
@@ -37,5 +25,5 @@ async def healthchecker(db: AsyncSession = Depends(get_db)):
             raise HTTPException(status_code=500, detail="Database is not configured correctly")
         return {"message": "Welcome to FastAPI!"}
     except Exception as e:
-        logger.error(f"Healthcheck error: {e}")
+        print(e)
         raise HTTPException(status_code=500, detail="Error connecting to the database")
